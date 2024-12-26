dff3dca2fb2deb9b483d2b8cbf8ada2e6b3185ae
Mr_Veit <Misster.veit@gmail.com>
2024-12-26 17:34:50 +1200

    First build

---
+++ index.js
@@ -0,0 +1,81 @@
+var unityInstanceRef;
+var unsubscribe;
+
+var container = document.querySelector("#unity-container");
+var progressBarContainer = document.querySelector(".progress-bar-container");
+var progressBar = document.querySelector(".progress-bar");
+var gameHeader = document.querySelector("#game-header");
+
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/TelegramMiniApp-MergeMemes-WebBuild.loader.js";
+var config = {
+    dataUrl: buildUrl + "/TelegramMiniApp-MergeMemes-WebBuild.data.unityweb",
+    frameworkUrl: buildUrl + "/TelegramMiniApp-MergeMemes-WebBuild.framework.js.unityweb",
+    codeUrl: buildUrl + "/TelegramMiniApp-MergeMemes-WebBuild.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Veiterio Games",
+    productName: "Merge Memes",
+    productVersion: "1.2"
+};
+
+function isMobile()
+{
+  return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+}
+
+if (isMobile())
+  {
+    // Mobile device style: fill the whole browser client area with the game canvas:
+    var meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  }
+
+progressBarContainer.style.display = "block";
+gameHeader.style.display = "block";
+
+var script = document.createElement("script");
+script.src = loaderUrl;
+
+script.onload = () =>
+{
+  createUnityInstance(document.querySelector("#unity-canvas"), config, (progress) =>
+  {
+    progressBar.style.width = (progress * 100) + "%";
+  })
+  .then((unityInstance) =>
+  {
+    unityInstanceRef = unityInstance;
+    window.unityInstance = unityInstance;
+
+    progressBarContainer.style.display = "none";
+    gameHeader.style.display = "none";
+  })
+  .catch((message) =>
+  {
+    console.error(message);
+  });
+};
+
+document.body.appendChild(script);
+
+window.addEventListener('load', function () 
+{
+  Telegram.WebApp.ready();
+  Telegram.WebApp.expand();
+
+  console.log("Telegram Web App has been expanded to full screen");
+
+  var version = Telegram.WebApp.version;
+  var versionFloat = parseFloat(version);
+
+  if (versionFloat >= 7.7) 
+  {
+    Telegram.WebApp.disableVerticalSwipes();
+
+    console.log('Activating vertical swipe disable');
+  }
+
+  console.log(`Telegram Web App opened with version: ${version}`);
+});
---
+++ serviceWorker.js
@@ -0,0 +1,97 @@
+let buildDirectory = "Build"
+let templateDataDir = "TemplateData";
+
+let cacheName = "HyyyyperCache";
+let buildVersion = "default";
+
+self.addEventListener("message", (event) =>
+{
+  if (event && event.data.type == "INIT_BUILD_INFO")
+  {
+    const buildInfo = event.data.payload;
+
+    cacheName = `${cacheName}_${buildInfo.version}_${buildInfo.buildInfo}`;
+
+    console.log(cacheName);
+
+    buildVersion = buildInfo.version;
+
+    console.log(`Service Worker initialized with cache name: ${cacheName}`);
+  }
+});
+
+self.addEventListener("install", function(event)
+{
+  console.log('service worker: install ' + buildVersion);
+
+  event.waitUntil(
+    caches.open(cacheName).then((cache) =>
+    {
+      return cache.addAll([
+          "./",
+          `index.html`,
+          `index.js`,
+          `buildConfigLoader.js`,
+          `${buildDirectory}/TelegramMiniApp-MergeMemes-WebBuild.loader.js`,
+          `${buildDirectory}/TelegramMiniApp-MergeMemes-WebBuild.data.unityweb`,
+          `${buildDirectory}/TelegramMiniApp-MergeMemes-WebBuild.framework.js.unityweb`,
+          `${buildDirectory}/TelegramMiniApp-MergeMemes-WebBuild.wasm.unityweb`,
+          `${templateDataDir}/Background.jpg`,
+          `${templateDataDir}/DesktopBackground.jpg`,
+          `${templateDataDir}/favicon.ico`,
+          `${templateDataDir}/ProgressBar-Background.png`,
+          `${templateDataDir}/ProgressBar-Fill.png`,
+          `${templateDataDir}/Fonts/Mont Heavy.otf`
+        ]);
+    })
+    .catch((error) =>
+    {
+      console.error('Failed to cache files:', error);
+    })
+  );
+});
+  
+self.addEventListener('activate', function(event) 
+{
+  console.log('service worker: Activate ' + buildVersion);
+
+  event.waitUntil(
+      caches.keys().then(function(keyList)
+      {
+        return Promise.all(
+          keyList.map(function(key)
+          {
+            if (key !== cacheName)
+            {
+              console.log('service worker: Removing old cache', key);
+
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+
+    return self.clients.claim();
+});
+  
+  self.addEventListener('message', function(event)
+  {
+    console.log('service worker: Message ' + buildVersion);
+
+    if (event.data.title && event.data.title === 'VersionTest')
+    {
+      if (event.data.version !== version)
+      {
+        messageClient(event.source, { title: 'ReplaceWorker' });
+      }
+      else
+      {
+        messageClient(event.source, { title: 'SameVersion' });
+      }
+    }
+  });
+  
+  let messageClient = async function(clientSource, message) {
+    clientSource.postMessage(message);
+  };
