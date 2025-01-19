e42a5486e19aebd013a11cf7273905faf496e33c
Mr_Veit <Misster.veit@gmail.com>
2025-01-20 06:19:10 +1200

    Fixed bug about massive spawn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Veiterio Games",
     productName: "Merge Memes",
-    productVersion: "1.2.5"
+    productVersion: "1.2.6"
 };
 
 function isMobile()
