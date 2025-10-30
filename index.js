68c6161c186f81ca83893a10a987555b3ff99464
Mr_Veit <74419352+MrVeit@users.noreply.github.com>
2025-10-30 13:55:56 +1200

    Updated project version to `6000.0.59f2` and more

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Veiterio Games",
     productName: "Merge Memes",
-    productVersion: "1.3.4"
+    productVersion: "1.3.5"
 };
 
 function isMobile()
